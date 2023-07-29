77b95664b423b231c53de9a952980e8b6393d96f
Zaki Elyasa Djauhari <zaki.djauhari@autopedia.id>
2023-07-29 16:39:47 +0700

    directory and category item component init

---
+++ src/components/category-item/category-item.component.tsx
@@ -0,0 +1,23 @@
+import './category-item.style.scss';
+
+type CategoryItemProps = {
+  title: string;
+  imageUrl: string;
+};
+
+const CategoryItem = ({ title, imageUrl }: CategoryItemProps) => {
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title.toUpperCase()}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
