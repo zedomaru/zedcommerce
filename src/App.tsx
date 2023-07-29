5de470e6e0eeefda65bf0f1993783a2bce67b799
Zaki Elyasa Djauhari <zaki.djauhari@autopedia.id>
2023-07-29 17:16:08 +0700

    implements routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
